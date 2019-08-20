9d7155a154a5127a407d3c1efd758e72410a0480
Ikeda Atsushi <a.ikd05@hotmail.co.jp>
2019-08-20 15:36:56 +0000

    :WIP: models/topic.rb case構文が機能していない？

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -4,16 +4,30 @@ class Topic < ApplicationRecord
   validates :deadline, presence: true  
   
 
-  def self.new_auto
-    topic = Topic.new(content: "Hello World", deadline: Time.current )
-    topic.save
-  end   
+  # def self.new_auto
+  #   topic = Topic.new(content: "Hello World", deadline: Time.current )
+  #   topic.save
+  # end   
+  
+  # def self.failure_auto
+  #   current_time_id = Time.current.strftime("%H%M")
+
+  #   failure_topic =  Topic.where(deadline_id: current_time_id)
+  #   failure_topic.status = "failure"
+  #   failure_topic.save
+  # end
+
   
   def self.failure_auto
-    failure_topic =  Topic.where(deadline_id: current_time_id)
-    failure_topic.status = "failure"
-    failure_topic.save
+    current_time_id = Time.current.strftime("%H%M")
+    failure_topics =  Topic.where(deadline_id: current_time_id)
+    failure_topics.each do |failure_topic|
+      case failure_topic.status
+      when "judging"
+        failure_topic.status = "failure"
+        failure_topic.save
+      end
+    end
   end
-
   
 end
